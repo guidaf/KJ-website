5b0fb334c29ebfdfd01a12e4d3c643843befbb6e
guidaf <guilhermefse@al.insper.edu.br>
2022-03-10 18:56:26 -0300

    Commit

--- app/router.js
+++ app/router.js
@@ -1,9 +1,29 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'kj-tabelas/config/environment';
+import { action } from '@ember/object';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('new-order');
+  this.route('orders');
+});
+
+export default class Tooltip extends Component {
+  @action
+  calculate() {
+    console.log('IM WORKING');
+  }
+  }
+
+
+// App.UsersRoute = Ember.Router.extend({
+//   model: function(){
+//     Ember.$.getCSV('//app/components/codigo-por-cep.csv')
+
+//   console.log(data)
+//   }
+// });
