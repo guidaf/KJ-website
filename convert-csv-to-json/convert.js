dd3a257a50ed7224a1606e0849910461c7725710
guidaf <guilhermefse@al.insper.edu.br>
2022-03-16 15:21:56 -0300

    limpeza

--- convert-csv-to-json/convert.js
+++ convert-csv-to-json/convert.js
@@ -1,18 +1,17 @@
 let csvToJson = require('convert-csv-to-json');
 
 let  fileInputName  =  'preco-por-codigo-e-peso.csv';  
-let  fileOutputName  =  'myOutputFile.json' ; 
+let  fileOutputName  =  'precoPorCodigo.json' ; 
 
-let json = csvToJson.getJsonFromCsv(fileInputName);
+// let json = csvToJson.getJsonFromCsv(fileInputName);
 
-for(let i=0; i<json.length;i++){
-    json[i].values = json[i].values.replace(",",".");
-
-}
+// for(let i=0; i<json.length;i++){
+//     var fileInput = Object.assign({},json[i]['peso-maximo'], json[i]['peso-maximo'].replace(",","."));
+// }
 
 csvToJson.formatValueByType().getJsonFromCsv(fileInputName);
 
-csvToJson.parseSubArray().getJsonFromCsv('preco-por-codigo-e-peso.csv');
+// csvToJson.parseSubArray().getJsonFromCsv('preco-por-codigo-e-peso.csv');
 
 csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
 
